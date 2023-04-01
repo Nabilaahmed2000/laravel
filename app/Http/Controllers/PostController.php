ad9b170f9b4138a6ed3c948be919f6260b8839f0
nabila_ahmed <nabilaahmed2022@gmail.com>
2023-04-01 22:22:17 +0200

    Commit message here

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+   private $allPosts = [
+        [
+            'id' => 1,
+            'title' => 'Laravel',
+            'description' => 'hello laravel',
+            'posted_by' => 'Nabila',
+            'created_at' => '2023-04-01 10:00:00',
+        ],
+
+        [
+            'id' => 2,
+            'title' => 'PHP',
+            'description' => 'hello php',
+            'posted_by' => 'Habiba',
+            'created_at' => '2023-04-01 10:00:00',
+        ],
+
+        [
+            'id' => 3,
+            'title' => 'Javascript',
+            'description' => 'hello javascript',
+            'posted_by' => 'Somaya',
+            'created_at' => '2023-04-01 10:00:00',
+        ],
+        [
+            'id' => 4,
+            'title' => 'BootStrap',
+            'description' => 'hello BootStrap',
+            'posted_by' => 'Mariam',
+            'created_at' => '2023-04-01 10:00:00',
+        ],
+    ];
+    public function index()
+    {
+        return view('posts.index', ['posts'=> $this -> allPosts]);
+    }
+    public function show($id)
+    {
+        $post = [
+            'id' => 1,
+                'title' => 'Laravel',
+                'description' => 'hello laravel',
+                'posted_by' => 'Nabila',
+                'created_at' => '2023-04-01 10:00:00'
+        ];
+
+        return view('posts.show', ['post' => $post]);
+    }
+public function create()
+    {
+
+        return view('posts.create');
+    }
+public function store()
+    {
+
+        return view('posts.index', ['posts'=> $this -> allPosts]);
+    }
+public function edit()
+    {
+        
+        return view('posts.edit');
+    }
+public function update()
+    {
+        //do some actionto update post
+        return view('posts.index', ['posts'=> $this -> allPosts]);
+    }
+public function delete()
+    {
+        //do some actionto delete post
+        return view('posts.index', ['posts'=> $this -> allPosts]);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/delete/{id}', [PostController::class, 'delete']) -> name('posts.delete');
+Route::get('/posts/edit/{id}', [PostController::class, 'edit']) -> name('posts.edit');
+Route::get('/posts/update', [PostController::class, 'update']) -> name('posts.update');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
